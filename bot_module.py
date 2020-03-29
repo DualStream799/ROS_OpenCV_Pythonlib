86927259e29125b119d8bc72d4a3ef62ec79c5c5
dualstream799 <dualstream799@gmail.com>
2020-03-29 01:17:55 -0300

    'TurtleBot' class detachment and inner import libraries available

--- bot_module.py
+++ bot_module.py
@@ -3,29 +3,35 @@
 
 __author__ = "DualStream799"
 
-# Importing ROS related Libraries:
-import rospy
-from sensor_msgs.msg import Image, CompressedImage  # for '/kamera' Subscriber
-from geometry_msgs.msg import Twist, Vector3        # for '/cmd_vel' Publisher
-from sensor_msgs.msg import LaserScan               # for '/scan' Subscriber
-from std_msgs.msg import UInt8                      # for '/bumper' Subscriber
 
-# Importing Computer Vision related Libraries:
+# Importing OpenCV related Libraries:
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 
 # Importing Support related Libraries:
+from heapq import nlargest, nsmallest
 import numpy as np
 import math
 import time
 
-class TurtleBot():
+class ControlBotModule():
     """docstring for TurtleBot"""
 
     def __init__(self):
+        # Importing ROS related Libraries:
+        import rospy
+        from sensor_msgs.msg import Image, CompressedImage  # for '/kamera' Subscriber
+        from geometry_msgs.msg import Twist, Vector3        # for '/cmd_vel' Publisher
+        from sensor_msgs.msg import LaserScan               # for '/scan'   Subscriber
+        from std_msgs.msg import UInt8                      # for '/bumper' Subscriber
+        # Setting imported libaries available outside '__init__' scope but inside 'ControlBotModule' scope (Imports only when the class is instanced and let them available to the others class' methods):
+        self.Image, self.CompressedImage = Image, CompressedImage
+        self.Twist, self.Vector3 = Twist, Vector3
+        self.LaserScan = LaserScan
+        self.UInt8 = UInt8
+
         # Instance needed to convert the images from "/kamera" to OpenCV especifications:
         self.cv_bridge = CvBridge()
-
 
         # Recieves the "/scan" Subscriber data:
         self.scan_data = []
@@ -34,13 +40,58 @@ class TurtleBot():
         # Recieves the "scan_data" element corresponding with frontal distance:
         self.ahead_fisrt = 0
         self.ahead_last = 0
-        # Standard speed for insert on "Vector3" objects:
+        # Standard speed for insert on 'Vector3' objects:
         self.linear_x = 0
         self.linear_y = 0
         self.linear_z = 0
         self.angular_x = 0
         self.angular_y = 0
         self.angular_z = 0
+
+    
+    # METHODS FOR DEALING WITH ROS DATA SENSORS:
+    def laser_scan(self, data):
+        """Deals with '/scan' Subscriber data"""
+        self.scan_data = np.array(data.ranges).round(decimals=2)
+        self.ahead_fisrt = self.scan_data[0]
+        self.ahead_last = self.scan_data[-1]
+
+
+    def bumper_scan(self, dado):
+        """Deals with '/bumper' Subscriber data"""
+        self.bumper = dado.data
+
+
+    def convert_compressed_to_cv2(self, image):
+        """Deals with '/kamera' Subscriber data"""
+        return self.cv_bridge.compressed_imgmsg_to_cv2(image, "bgr8")
+
+
+    # METHODS FOR ROS MOVIMENTATION:
+    def main_twist(self):
+        """Returns a Twist object with all the linear and algular values on it"""
+        return self.Twist(self.Vector3(self.linear_x, self.linear_y, self.linear_z), self.Vector3(self.angular_x, self.angular_y, self.angular_z))
+
+
+    def run_bot(self, execute_method):
+        """Executes an algorithm based on the available ones"""
+        if __name__ == "__main__":
+            rospy.init_node("bot_module")
+            try:
+                while not rospy.is_shutdown():
+                    if self.change_key == False:
+                        execute_method()
+                    else:
+                        select_mode = 0
+
+            except rospy.ROSInterruptException:
+                print("Ocorreu uma exceção com o rospy")
+
+
+class VisionBotModule():
+    """docstring for VisionBotModule"""
+    def __init__(self):
+        pass
 
     # METHODS FOR COMPUTER VISION PROCESSMENTS:
     def frame_flip(self, frame, flip_mode):
@@ -83,37 +134,37 @@ class TurtleBot():
         cv2.line(rgb_frame, (point[0] - length/2, point[1]), (point[0] + length/2, point[1]), color, width, length)
         cv2.line(rgb_frame, (point[0], point[1] - length/2), (point[0], point[1] + length/2), color, width, length) 
     
+    def display_text(self, frame, text, position, thickness, font_size=1, text_color=(255, 255, 255), shadow_color=(128, 128, 128), font_style=cv2.FONT_HERSHEY_SIMPLEX, line_style=cv2.LINE_AA):
+        """Displays a text on the frame with a shadow behind it for better visualization on any background"""
+        cv2.putText(frame, text, position, font_style, font_size, shadow_color, thickness+1, line_style)
+        cv2.putText(frame, text, position, font_style, font_size, text_color, thickness, line_style)
 
-    # METHODS FOR DEALING WITH ROS DATA SENSORS:
-    def laser_scan(self, data):
-        """Deals with "/scan" Subscriber data"""
-        self.scan_data = np.array(data.ranges).round(decimals=2)
-        self.ahead_fisrt = self.scan_data[0]
-        self.ahead_last = self.scan_data[-1]
 
-    def bumper_scan(self, dado):
-        """Deals with "/bumper" Subscriber data"""
-        self.bumper = dado.data
+class SupportBotModule():
+    """docstring for SupportBotModule"""
+    def __init__(self):
+        pass
+    
+    def angular_coefficient(self, point1, point2, decimals=0, to_degrees=False):
+        """Calculates the angular coefficient if a line between two points using the current formula: (y - y0) = m*(x - x0)"""
+        m = (point2[1] - point1[1])/(point2[0] - point1[0])
+        if to_degrees == False:
+            return m
+        else:
+            return round(degrees(atan(m)), decimals)
 
-    def convert_compressed_to_cv2(self, image):
-        """Deals with '/kamera' Subscriber data"""
-        return self.cv_bridge.compressed_imgmsg_to_cv2(image, "bgr8")
+    def calculate_vanishing_point(self, p1, p2, q1, q2):
+        """Calculates vanishing point based in the intersection of two lines"""
+        def calculate_h(point, m):
+            return point[1] - m*point[0]
 
-    # METHODS FOR ROS MOVIMENTATION:
-    def main_twist(self):
-        """Returns a Twist object with all the linear and algular values on it"""
-        return Twist(Vector3(self.linear_x, self.linear_y, self.linear_z), Vector3(self.angular_x, self.angular_y, self.angular_z))
-
-    def run_bot(self, execute_method):
-        """Executes an algorithm based on the available ones"""
-        if __name__ == "__main__":
-            rospy.init_node("bot_module")
-            try:
-                while not rospy.is_shutdown():
-                    if self.change_key == False:
-                        execute_method()
-                    else:
-                        select_mode = 0
-
-            except rospy.ROSInterruptException:
-                print("Ocorreu uma exceção com o rospy")
+        m1 = self.angular_coefficient(p1, p2)
+        m2 = self.angular_coefficient(q1, q2)
+        
+        h1 = calculate_h(p1, m1)
+        h2 = calculate_h(q1, m2)
+        
+        xi = (h2 - h1)/(m1 - m2)
+        yi = m1*xi + h1
+        
+        return (int(xi), int(yi))
